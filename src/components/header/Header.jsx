46c258dfecaf1af96d4923bc73e345a64ac7f5a3
amshaaa <amishachhetri426@gmail.com>
2025-05-22 17:56:27 +0545

    heading

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,8 @@ const Header = () => {
 
   const links = [
     { name: "Home", path: "/" },
-    { name: "Bikes For Rent", path: "/booking" },
+    
+    { name: "Our Services", path: "/services" },
     { name: "About Us", path: "/about" },
     { name: "FAQS", path: "/faqs" },
   ];
@@ -20,50 +21,59 @@ const Header = () => {
       <nav className="bg-[#fffafa] ">
         <div className="wrapper">
           <div className="flex items-center justify-between font-poppins px-4 py-5">
-          <div className="text-[27px] font-bold text-[#025CA3]">
-            Easy <span className="text-[#025CA3]">RentAL</span>
-          </div>
+            <div className="text-[27px] font-bold text-[#025CA3]">
+              Easy <span className="text-[#025CA3]">RentAL</span>
+            </div>
 
-          {/* Desktop Menu */}
+            {/* Desktop Menu */}
 
-          <div className="hidden md:flex gap-6 items-center ">
-            <ul className="hidden md:flex gap-6">
-              {links.map((link, index) => (
-                <li key={index}>
-                  <Link
-                    to={link.path}
-                    className="text-black font-semibold hover:text-[#025CA3] transition-colors"
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <div className="hidden md:flex gap-6 items-center ">
+              <ul className="hidden md:flex gap-6">
+                {links.map((link, index) => (
+                  <li key={index}>
+                    <Link
+                      to={link.path}
+                      className="text-black font-semibold hover:text-[#025CA3] transition-colors"
+                    >
+                      {link.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
 
-            <div className="flex flex-row gap-6">
-              <button onClick={() => navigate("/login")} className="px-5 py-2 border hover:cursor-pointer border-[#025CA3]  text-[#025CA3] font-semibold rounded-md hover:bg-blue-200">Login</button>
-              <button onClick={() => navigate("/Registration")} className="px-5 py-2 hover:cursor-pointer border border-[#025CA3] text-[#025CA3] font-semibold rounded-md hover:bg-blue-200">Signup</button>
+              <div className="flex flex-row gap-6">
+                <button
+                  onClick={() => navigate("/login")}
+                  className="px-5 py-2 border hover:cursor-pointer border-[#025CA3]  text-[#025CA3] font-semibold rounded-md hover:bg-blue-200"
+                >
+                  Login
+                </button>
+                <button
+                  onClick={() => navigate("/Registration")}
+                  className="px-5 py-2 hover:cursor-pointer border border-[#025CA3] text-[#025CA3] font-semibold rounded-md hover:bg-blue-200"
+                >
+                  Signup
+                </button>
+              </div>
+            </div>
+
+            {/* Hamburger Icon */}
+
+            <div className="md:hidden">
+              <button onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? (
+                  <IoClose className="cursor-pointer" size={30} />
+                ) : (
+                  <HiMenu className="cursor-pointer" size={30} />
+                )}
+              </button>
             </div>
           </div>
 
+          {/* Mobile Nav */}
 
-
-          {/* Hamburger Icon */}
-
-          <div className="md:hidden">
-            <button onClick={() => setIsOpen(!isOpen)}>
-              {isOpen ? <IoClose className="cursor-pointer" size={30} /> : <HiMenu className="cursor-pointer" size={30} />}
-            </button>
-          </div>
-        </div>
-
-
-        {/* Mobile Nav */}
-
-        {
-          isOpen && (
+          {isOpen && (
             <>
-
               <ul className="flex md:hidden flex-col items-center justify-center gap-4 px-4 pb-4 ">
                 {links.map((link, index) => (
                   <li key={index}>
@@ -78,13 +88,21 @@ const Header = () => {
               </ul>
 
               <div className="flex md:hidden flex-col items-center justify-center gap-4 px-4 pb-4">
-                <button onClick={() => navigate("/login")} className="px-5 py-2 border hover:cursor-pointer border-[#025CA3] text-[#025CA3] font-semibold rounded-md hover:bg-blue-200">Login</button>
-                <button onClick={() => navigate("/Registration")} className="px-5 py-2 hover:cursor-pointer border border-[#025CA3] text-[#025CA3] font-semibold rounded-md hover:bg-blue-200">Signup</button>
+                <button
+                  onClick={() => navigate("/login")}
+                  className="px-5 py-2 border hover:cursor-pointer border-[#025CA3] text-[#025CA3] font-semibold rounded-md hover:bg-blue-200"
+                >
+                  Login
+                </button>
+                <button
+                  onClick={() => navigate("/Registration")}
+                  className="px-5 py-2 hover:cursor-pointer border border-[#025CA3] text-[#025CA3] font-semibold rounded-md hover:bg-blue-200"
+                >
+                  Signup
+                </button>
               </div>
-
             </>
-          )
-        }
+          )}
         </div>
       </nav>
     </>
--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,21 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import Booking from "../pages/Booking";
+
 import Details from "../pages/Details";
 
 import Faqs from "../pages/Faqs.jsx";
 import About from "../pages/About.jsx";
 import Login from "../pages/Login.jsx";
 import Registration from "../pages/Registration.jsx";
-import Bookingconfirmation from '../pages/Bookingconfirmed.jsx'
-
+import Bookingconfirmation from "../pages/Bookingconfirmed.jsx";
+import OurServices from "../pages/OurServices.jsx";
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/booking" element={<Booking />} />
-
      
+
       <Route path="/faqs" element={<Faqs />} />
       <Route path="/about" element={<About />} />
       <Route path="/Login" element={<Login />} />
@@ -23,10 +22,7 @@ const Router = () => {
 
       <Route path="/details/:id" element={<Details />} />
       <Route path="/confirmation" element={<Bookingconfirmation />} />
-       
-         
-
-
+      <Route path="/services" element={<OurServices />} />
     </Routes>
   );
 };
